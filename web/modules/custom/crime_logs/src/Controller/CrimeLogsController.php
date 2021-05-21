2109ed3d18b6caf0810630566bd8455d69f15b3d
Grady Wring <grady@gradywring.com>
2021-05-21 13:33:07 -0500

    Try crime log as iframe.

--- web/modules/custom/crime_logs/src/Controller/CrimeLogsController.php
+++ web/modules/custom/crime_logs/src/Controller/CrimeLogsController.php
@@ -27,66 +27,11 @@ class CrimeLogsController extends ControllerBase {
 
     return [
       '#type' => 'markup',
-      '#markup' => $content,
+      '#markup' => '<iframe src="http://www.police.gatech.edu/crimelog.php" height="100%" width="100%" style="width:100%; height:1000px; border:none;"></iframe>',
       '#cache' => [
           'max-age' => 0,
       ],
     ];
-  }
-
-  private function getContent() {
-    $urlprefix = "http://reports.police.gatech.edu/public/crimelog.asp";
-
-    //sanitize user input
-    if ($_GET['offset'] == strval(intval($_GET['offset'])))
-        {
-            $offsetsuffix = $_GET['offset'];
-        }
-    
-    if ($_GET['OCA'] == strval(intval($_GET['OCA'])))
-        {
-            $OCAsuffix = $_GET['OCA'];
-        }
-    
-    // OCA can not be negative, offset = -1 is used to go to last page, any other negative numbers are not used.
-    if ($offsetsuffix < -1)
-        {
-            $offsetsuffix = 0;
-        }
-    
-    if ($OCAsuffix < 0)
-        {
-            $OCAsuffix = 0;
-        }
-    
-    //Create url values to be passed to reports.police.gatech.edu if requested, skipped if values were not passed.
-    
-    if (isset($offsetsuffix))
-        {
-            $offsetsuffix = "offset=".$offsetsuffix;
-        }
-    
-    
-    if (isset($OCAsuffix))
-        {
-            $OCAsuffix = "OCA=".$OCAsuffix;
-        }
-    
-    //create final url. This adds "?" and "&" if needed
-    $url = $urlprefix;
-    
-    if ((isset($OCAsuffix)) || (isset($offsetsuffix)))
-        {
-            $url = $urlprefix."?".$OCAsuffix.$offsetsuffix;
-        }
-    
-    if ((isset($OCAsuffix)) && (isset($offsetsuffix)))
-        {
-            $url = $urlprefix."?".$OCAsuffix."&".$offsetsuffix;
-        }
-    
-    return readfile($url);
-        
   }
   
 }
